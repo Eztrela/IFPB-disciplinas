d5c8933b92f649781fa8c08be489988e1bfb8c53
Pablo <pablo.estrela@gmail.com>
2023-03-15 11:17:09 -0300

    update Venda.java

--- POO/Slide03/Venda.java
+++ POO/Slide03/Venda.java
@@ -1,11 +1,13 @@
 
 public class Venda {
 	private String data;
+	private String nome;
 	private int quantidade;
 	private double valor;
 	public Venda(String data,Produto produto, int quantidade) {
 		this.data = data;
 		this.quantidade = quantidade;
+		this.nome = produto.getNome()
 		this.valor = quantidade * produto.getValor();
 		produto.setestoque(produto.getEstoque() -  this.quantidade)
 		
