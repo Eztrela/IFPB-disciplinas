20a84f1b23ba9cb59d81d3528971640e881e6b70
Pablo <pablo.estrela@gmail.com>
2023-03-15 11:14:16 -0300

    update Venda.java

--- POO/Slide03/Venda.java
+++ POO/Slide03/Venda.java
@@ -1,14 +1,13 @@
 
 public class Venda {
 	private String data;
-	private Produto produto;
 	private int quantidade;
 	private double valor;
-	public Venda(String data, int quantidade) {
+	public Venda(String data,Produto produto, int quantidade) {
 		this.data = data;
 		this.quantidade = quantidade;
-		this.valor = calculaValor();
-		this.decrementaEstoque();
+		this.valor = quantidade * produto.getValor();
+		produto.setestoque(produto.getEstoque() -  this.quantidade)
 		
 	}
 	
@@ -17,13 +16,13 @@ public class Venda {
 		return "Venda [data=" + data + ", produto=" + produto + ", quantidade=" + quantidade + ", valor=" + valor + "]";
 	}
 
-	public double calculaValor() {
-		return produto.getPreco() * quantidade;
-	}
+//	public double calculaValor() {
+//		return produto.getPreco() * quantidade;
+//	}
 	
-	public void decrementaEstoque(Produto produto) {
-		int estoqueAtualizado = produto.getEstoque() -  quantidade;
-		produto.setEstoque(estoqueAtualizado);
-	}
+//	public void decrementaEstoque(Produto produto) {
+//		int estoqueAtualizado = produto.getEstoque() -  quantidade;
+//		produto.setEstoque(estoqueAtualizado);
+//	}
 
 }
